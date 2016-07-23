edda08d4ae0db77e9f6997972b714ffeee79761e
ryan pythonany <ryan@nolaupdate.com>
2016-07-23 16:33:29 +0000

    after first makemigrations

--- update_planet/update_planet/settings.py
+++ update_planet/update_planet/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'planet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'tagging',
+    'pagination'
 ]
 
 MIDDLEWARE_CLASSES = [
